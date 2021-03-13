84f1c5c8ef98fe521e831823234f4921778bacf7
GINTARAS JASAITIS <gintaras.jasaitis@gmail.com>
2021-03-13 11:18:24 +0200

    Cleaned setup.

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+	apiKey: "AIzaSyBv071d4q7i9vsg0uILSSPKQpLrpNJhhK8",
+	authDomain: "fb-clone-a80c0.firebaseapp.com",
+	databaseURL: "https://fb-clone-a80c0.firebaseio.com",
+	projectId: "fb-clone-a80c0",
+	storageBucket: "fb-clone-a80c0.appspot.com",
+	messagingSenderId: "523275562182",
+	appId: "1:523275562182:web:73fbe3846b1a53b1e8fbc7",
+	measurementId: "G-6887S5NJQ9"
+  };
